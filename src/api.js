5d5c0281c882fb7b0ec2dad4daaf997ceab5a962
lucas Meregali <lucas.smeregalli@gmail.com>
2023-05-29 11:35:54 -0300

    mongodb ajustes

--- src/api.js
+++ src/api.js
@@ -11,50 +11,54 @@ app.use(express.urlencoded({
 
 const router = express.Router();
 
-app.use('/', router.get('/', (req, res,next) => {
+app.use('/', router.get('/', (req, res, next) => {
     res.send("API OK");
-    }));
+}));
 
-app.use('/',router.get('/sobre' , (req, res,next) => { 
+app.use('/', router.get('/sobre', (req, res, next) => {
     res.send({
-        "nome":"API de Chat",
-        "versao":"1.0.0",
-        "status":"em produção"
+        "nome": "API de Chat",
+        "versao": "1.0.0",
+        "status": "em produção"
     });
-    }));
+}));
 
-app.use('/entrar', router.post('/entrar', async (req, res,next) => {
+app.use('/entrar', router.post('/entrar', async (req, res, next) => {
     let resp = await usuarioController.entrar(req.body.nick);
-    if(resp){
+    if (resp) {
         res.send(resp);
-    }else{
-        res.send({"msg":"erro"});
+    } else {
+        res.send({
+            "msg": "erro"
+        });
     }
 }));
 
-app.use('/salas', router.get('/salas', async (req, res,next) => {
-    if(await tokenUtil.checktoken(req.headers.token, req.headers.idUser, req.headers.timestamp)){
+app.use('/salas', router.get('/salas', async (req, res, next) => {
+    if (await tokenUtil.checktoken(req.headers.token, req.headers.idUser, req.headers.timestamp)) {
         let resp = await salaController.get();
         res.send(resp);
-    }else{
-        res.send({"msg":"erro, usuário não autenticado"});
+    } else {
+        res.send({
+            "msg": "erro, usuário não autenticado"
+        });
     }
 }));
 
-app.use('/salas/entrar', router.get('/salas/entrar', async (req, res,next) => {
-    if(await tokenUtil.checktoken(req.headers.token, req.headers.iduser, req.headers.nick))
+app.use('/salas/entrar', router.get('/salas/entrar', async (req, res, next) => {
+    if (await tokenUtil.checktoken(req.headers.token, req.headers.iduser, req.headers.nick))
         return false;
 
-        let resp = await salaController.entrar(req.headers.idUser, req.query.idSala);
-        req.send(resp);
+    let resp = await salaController.entrar(req.headers.idUser, req.query.idSala);
+    req.send(resp);
 }));
 
-app.use('/sala/mensagens', router.get('/sala/mensagens', async (req, res,next) => {
-    if(!token.checktoken(req.headers.token, req.headers.iduser, req.headers.nick))
+app.use('/sala/mensagens', router.get('/sala/mensagens', async (req, res, next) => {
+    if (!tokenUtil.checktoken(req.headers.token, req.headers.iduser, req.headers.nick))
         return false;
 
-        let resp = await salaController.buscarMensagens(req.query.idSala, req.query.timestamp);
-        req.send(resp);
+    let resp = await salaController.buscarMensagens(req.query.idSala, req.query.timestamp);
+    req.send(resp);
 }));
-        
-    module.exports = app;
+
+module.exports = app;
